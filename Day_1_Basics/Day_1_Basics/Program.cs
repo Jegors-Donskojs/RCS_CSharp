b54e54e0f4c53323e099420480a9763a47202271
LockRage <60352986+LockRage@users.noreply.github.com>
2020-01-27 22:56:14 +0200

    Create Program.cs

---
+++ Day_1_Basics/Day_1_Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_1_Basics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
